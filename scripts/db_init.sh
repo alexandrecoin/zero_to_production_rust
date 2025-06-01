ac2e340fa88a254547440ed20d1f20365c1429bb
Alexandre COIN <alexandrecoin@mac.home>
2025-06-01 18:19:35 +0200

    tech: add PG database App User

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -6,6 +6,9 @@ set -eo pipefail
 DB_PORT="${DB_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
 SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+APP_USER="${APP_USER:=app}"
+APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
 # Launch PG using Docker
 CONTAINER_NAME="zero2prod_postgres"
@@ -16,3 +19,11 @@ docker run \
 --detach \
 --name "${CONTAINER_NAME}" \
 postgres -N 1000
+
+# Create the application user
+CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+
+# Grant DB privileges to the App user
+GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
